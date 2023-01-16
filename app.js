9c6c7ac6fd736931c4c33a8d2ec1e24cb6fe35fa
Therese Hagwall <90thejag@skola.boras.se>
2023-01-16 11:43:28 +0100

    Add object

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 "use strict";
 const arr = ["apa", "orangutang", "gorilla", "get", "giraff"];
 console.log(arr);
+const obj = {
+    name: "Therese",
+    age: 32,
+    hasDriversLicens: true
+};
+console.log(obj);
--- app.ts
+++ app.ts
@@ -1,3 +1,11 @@
 const arr: string[] = ["apa", "orangutang", "gorilla", "get", "giraff"]
 
 console.log(arr);
+
+const obj: { name: string; age: number; hasDriversLicens: boolean } = {
+    name: "Therese",
+    age: 32,
+    hasDriversLicens: true
+};
+
+console.log(obj);
